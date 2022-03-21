346e0711b6dfac90518395189c0a629ba5281e7e
koladeg <koladegureje@gmail.com>
2022-03-21 11:44:17 +0100

    initial commit

---
+++ src/data/SliderData.js
@@ -0,0 +1,75 @@
+import ImageOne from '../images/house 0.png'
+import ImageTwo from '../images/house 1.png'
+import ImageThree from '../images/house 2.png'
+import ImageFour from '../images/house3.png'
+import ImageFive from '../images/house 4.png'
+
+export const SliderData = [
+    {
+        title: 'Estate villa in Ibadan, Akobo',
+        price: '₦6,500,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House' 
+    },
+    {
+        title: 'House in Ibadan, Challenge',
+        price: '₦2,950,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House' 
+    },
+    {
+        title: 'House in Ibadan, Bodija',
+        price: '₦8,400,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House' 
+    },
+    {
+        title: 'House in Ibadan, Onireke',
+        price: '₦6,660,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House' 
+    },
+];
+
+export const SliderDataTwo = [
+    {
+        title: 'Estate villa in Ibadan, Akobo',
+        price: '₦6,500,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House' 
+    },
+    {
+        title: 'Estate villa in Ibadan, Akobo',
+        price: '₦6,500,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House' 
+    },
+    {
+        title: 'Estate villa in Ibadan, Akobo',
+        price: '₦6,500,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House' 
+    },
+    {
+        title: 'Estate villa in Ibadan, Akobo',
+        price: '₦6,500,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House' 
+    },
+];
